f548248fe90be055a05c9933d44b526490d28291
James Homer <jameshomer85@gmail.com>
2016-07-04 13:58:47 +0100

    webpack fix

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-require('angular');
-require('gsap');
-// require('scrollmagic');
-// require('scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap');
-// require('scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators');
 require('./angular-scroll-magic');
+
 module.exports = 'hj.scrollMagic';
-
-// (function (root, factory) {
-//   'use strict';
-//   if (typeof define === 'function' && define.amd) {
-//     // AMD
-//     define([
-//       'angular',
-//     ], factory);
-
-//   } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
-//     // CommonJS
-//     module.exports = factory.apply(root, [
-//       require('angular'),
-//     ]);
-
-//   } else {
-//     return factory(root.angular);
-//   }
-// } (this, function (angular) {
-//   'use strict';
-
-//   // module here
-
-//   return 'moduleName';
-// }));
---
+++ index.webpack.js
@@ -0,0 +1,8 @@
+require('angular');
+require('gsap');
+require('script!scrollmagic');
+require('script!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap');
+require('script!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators');
+require('./angular-scroll-magic');
+
+module.exports = 'hj.scrollMagic';
